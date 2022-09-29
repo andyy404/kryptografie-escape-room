18c1f0c1aa8a80efbe2c6370ed363e5b6cbe5a7b
andyy404 <97410799+andyy404@users.noreply.github.com>
2022-09-29 21:36:18 +0200

    main.py korrigiert

--- source/main.py
+++ source/main.py
@@ -15,17 +15,52 @@ while Setup.running:
 
     if Setup.scene == 0: # renders the main menu
         Setup.menu.update()
+        
     if Setup.scene == 1: # if this is true, it means you are in game
         Setup.room.draw_lines()
         Setup.room.draw_objects()
+
+        Setup.room.check_hitboxes()
+
         Setup.uv_lamp.update()
-        Setup.cipher_wheel.update()
-        Setup.pause.update()
+        Setup.cipher_wheel.check()
+
+        Setup.room.draw_uv_things()
+        
+        Setup.pause.button()
+
         Setup.timer.update()
+        Setup.timer.draw()
+
     if Setup.scene == 2:
         Setup.end_screen.update(True)
+        Setup.timer.draw()
+
     if Setup.scene == 3:
         Setup.end_screen.update(False)
+
+    if Setup.scene == 4:
+        Setup.pause.update()
+        Setup.timer.draw()
+
+    if Setup.scene == 5:
+        Setup.cipher_wheel.update()
+        Setup.timer.update()
+        Setup.timer.draw()
+
+    if Setup.scene == 6:
+        Setup.room.active_interaction_screen()
+
+        Setup.uv_lamp.update()
+        Setup.cipher_wheel.check()
+
+        Setup.room.draw_uv_things()
+
+        Setup.back_button()
+
+        Setup.timer.update()
+        Setup.timer.draw()
+
 
     if Setup.timer.time < 0:
         Setup.scene = 3
